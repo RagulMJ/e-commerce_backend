27fe015ae2960e3bbf404b1903d5a88c7ff2487a
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-09-26 18:49:37 +0530

    amazon-backend

--- server.js
+++ server.js
@@ -15,19 +15,18 @@ app.use(cors());
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-// app.get('api/products/slug/:slug', (req, res) => {
-//   const slug = req.params.slug;
-//   const product = data.products.find((x) => x.slug === slug);
-//   console.log(product);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: 'Product Not Found' });
-//   }
-// });
 
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
